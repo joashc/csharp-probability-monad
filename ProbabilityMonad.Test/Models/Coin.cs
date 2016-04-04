2c007b29745d104d59e443895f2a043defecb853
joashc <joash.is@gmail.com>
2016-04-04 18:16:41 +1000

    Add coin model

---
+++ ProbabilityMonad.Test/Models/Coin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ProbabilityMonad.Base;
+
+namespace ProbabilityMonad.Test.Models
+{
+    public static class CoinExt
+    {
+        public static Coin Heads = new Coin(true);
+
+        public static Coin Tails = new Coin(false);
+
+        /// <summary>
+        /// Update weight distribution based on one coin flip
+        /// </summary>
+        public static Func<Coin, Dist<double>, Dist<double>>
+        FlipUpdate = (coin, dist) => Condition(w => Prob(coin.Heads ? w : 1 - w), dist);
+
+        /// <summary>
+        /// Update weight distribution based on a series of coin flips
+        /// </summary>
+        public static Func<List<Coin>, Dist<double>, Dist<double>>
+        FlipsUpdate = (coins, dist) => coins.Aggregate(dist, (d, t) => FlipUpdate(t, d));
+
+        /// <summary>
+        /// Update exact weight distribution based on one coin flip
+        /// </summary>
+        public static Func<Coin, FiniteDist<double>, FiniteDist<double>>
+        FlipUpdateExact = (coin, dist) => dist.ConditionSoft(w => Prob(coin.Heads ? w : 1 - w));
+
+        /// <summary>
+        /// Update exact weight distribution based on a series of coin flips
+        /// </summary>
+        public static Func<List<Coin>, FiniteDist<double>, FiniteDist<double>>
+        FlipsUpdateExact = (coins, dist) => coins.Aggregate(dist, (d, t) => FlipUpdateExact(t, d));
+    }
+
+    public class Coin
+    {
+        public Coin(bool heads)
+        {
+            Heads = heads;
+        }
+
+        public bool Heads;
+        public bool Tails { get { return !Heads; } }
+    }
+}
